e698132b8f1cfc608787f2bff7108158236fa05a
reeteshc <reeteshkumarchrs7@gmail.com>
2026-05-09 19:17:44 +0530

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express ()
 const PORT = process.env.PORT   ?? 8080
 app.get('/', (req, res) => {
-return res.json({msg: 'Hello this node js app deployed by github actions on AWS EC2 CICD pipeline'})
+return res.json({msg: 'Hello this node js app deployed by github actions on AWS EC2 CICD'})
 })
  
 app.listen(PORT, () => {
