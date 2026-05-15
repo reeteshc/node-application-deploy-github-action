402202cd719dc9e38ef7223d1c2bb6a05e79032f
reeteshc <reeteshkumarchrs7@gmail.com>
2026-05-15 14:59:09 +0530

    tttttt

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ import express from 'express'
 const app = express ()
 const PORT = process.env.PORT   ?? 8080
 app.get('/', (req, res) => {
-return res.json({msg: 'Hello this node js app'})
+return res.json({msg: 'Hello this node js app 2'})
 })
  
 app.listen(PORT, () => {
     console.log ('The Server is up and Running on PORT  ${PORT}')
 })
-
-
-
